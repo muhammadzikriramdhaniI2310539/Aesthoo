ed80df85c43e59b9dda54655ba385e3e44562e18
Muhammad Dzikri Ramdhani I.2310539 <dzikriramdhani91@gmail.com>
2026-07-03 09:56:46 +0700

    main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { Analytics } from '@vercel/analytics/react'
+import { inject } from '@vercel/analytics'
+
+if (import.meta.env.PROD) {
+  inject()
+}
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-    <Analytics />
   </React.StrictMode>,
 )
